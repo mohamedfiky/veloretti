385d1ecc4db97c73fa8c986d3db7dc735063f5ff
Mohamed El-Fiky <fiky.link@gmail.com>
2025-11-25 12:36:43 +0200

    create Hero component & add some images

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+
+function Hero() {
+  return (
+    <div className="bg-[#948E82] h-screen">Hero</div>
+  )
+}
+
+export default Hero;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
+import Hero from "../components/hero";
+
 function Home() {
   return (
-    <h1>Home page</h1>
+    <>
+      <Hero />
+    </>
   )
 }
 
