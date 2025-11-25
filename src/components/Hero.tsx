a94124c1d8fabedeb3e570ed0a37dccc6effd4d0
Mohamed El-Fiky <fiky.link@gmail.com>
2025-11-25 23:27:38 +0200

    reduce images size using Magick and tiny.png

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,11 @@
+import heroImage from "../assets/images/hero3.png" ;
 
 function Hero() {
   return (
-    <div className="bg-[#948E82] h-screen">Hero</div>
+    <div className="bg-[#948E82] h-[calc(100vh-50px)] md:h-[calc(100vh-44px)]">
+      {/* حط الصورة جوا div وإبقى إتحكم فى مساحته براحتك عشان تظبط ال responsive */}
+      <img src={heroImage} className=" h-full w-full object-contain" />
+    </div>
   )
 }
 
